cc07f5e03b6f99b441a7c26a11d16002e05652a4
Faizal <94194196+Faizal-Malek@users.noreply.github.com>
2025-05-13 21:37:49 +0200

    Update server.ts

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -20,8 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
-  origin: 'https://urban-tokenization-survey.vercel.app',
-  credentials: true
+  origin: "https://urban-tokenization-survey.vercel.app",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(cors({
   origin: ["https://urban-tokenization-survey.vercel.app"],
